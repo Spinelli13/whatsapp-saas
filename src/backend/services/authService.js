73b78107ef5cbd5a97896e0cfa9960630cab0b54
Spinelli13 <matheus.cspinelli@gmail.com>
2026-07-07 12:02:14 -0300

    [2.3-fix] Substituir bcrypt por bcryptjs (compatibilidade Docker)

--- src/backend/services/authService.js
+++ src/backend/services/authService.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { Usuario, Cliente } = require('../models');
 const { JWT_SECRET, JWT_EXPIRES_IN } = require('../config/environment');
